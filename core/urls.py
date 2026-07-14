8aedb5f5e29c917197dc04b5376576ec0e0c54ac
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-14 16:23:52 +0530

    added urls

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from core.views import *
 
 urlpatterns = [
-    path('', DashboardAPI.as_view(), name='dashboard'),
+    path('dashboard/', DashboardAPI.as_view(), name='dashboard'),
     path('quiz/<int:pk>/<int:ck>/', QuizAPI.as_view(), name='quiz'),
 ]
